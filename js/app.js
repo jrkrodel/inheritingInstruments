b5722e41b7a054a5d14ae20989dfe9d588002c15
jrkrodel <78804328+jrkrodel@users.noreply.github.com>
2022-09-07 19:10:23 -0400

    updated instrument class

--- js/app.js
+++ js/app.js
@@ -8,17 +8,14 @@ class Instrument {
     this.loudness = loudness;
     this.family = family;
     this.play = play;
+    this.over = playNext;
   }
 
   playSound(duration) {
     console.log(`${this.family} ${this.play} at ${this.loudness}`);
     const synth = new Tone.Synth().toDestination();
     synth.triggerAttackRelease("A4", duration);
-    setTimeout(this.over.bind(this), duration * 1000);
-  }
-
-  over() {
-    playNext();
+    setTimeout(this.over, duration * 1000);
   }
 }
 
@@ -51,18 +48,6 @@ const guitar = new String(20);
 const flute = new Woodwind(10);
 
 const instruments = [drums, guitar, flute];
-
-//I found a way to get the playNext/Loop to work together but kept everything like how I showed in class
-//If I wanted to use the playSounds function I would just change out the button to
-//use it instead of playNext() and change the "over" method in the instrument class to no longer call playNext()
-
-// function playSounds() {
-//   for (let index = 0; index < instruments.length; index++) {
-//     setTimeout(() => {
-//       playNext();
-//     }, 500 * index);
-//   }
-// }
 
 function playNext() {
   if (currentInstrument < instruments.length) {
